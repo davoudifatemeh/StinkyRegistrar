399952f5dce99db918457eb4362470f4032e4a92
Fatemeh Davoudi <70088942+davoudifatemeh@users.noreply.github.com>
2021-06-21 21:14:29 +0430

    Update EnrollCtrl.java
    Define a string variable named msg in checkUnitsLimit, checkDuplicateEnrollment, checkExamTime, checkPrerequisites and checkCourseIsPassed  functions and use this string as exception messege

--- src/domain/EnrollCtrl.java
+++ src/domain/EnrollCtrl.java
@@ -1,7 +1,6 @@
 package domain;
 
 import java.util.List;
-import java.util.Map;
 
 import domain.exceptions.EnrollmentRulesViolationException;
 
@@ -12,7 +11,6 @@ public class EnrollCtrl {
     }
 
     private void checkEnrollmentConditions(Student student, List<CSE> courses) throws EnrollmentRulesViolationException {
-        Map<Term, Map<Course, Double>> transcript = student.getTranscript();
         for (CSE offering : courses) {
             checkCourseIsPassed(student, offering);
             checkPrerequisites(student, offering);
@@ -24,11 +22,12 @@ public class EnrollCtrl {
 
     private void checkUnitsLimit(List<CSE> courses, Student student) throws EnrollmentRulesViolationException {
         int unitsRequested = calcRequestedUnits(courses);
+        String msg = "Number of units (%d) requested does not match GPA of %f";
         double gpa = student.getGPA();
         if ((gpa < 12 && unitsRequested > 14) ||
                 (gpa < 16 && unitsRequested > 16) ||
                 (unitsRequested > 20))
-            throw new EnrollmentRulesViolationException(String.format("Number of units (%d) requested does not match GPA of %f", unitsRequested, gpa));
+            throw new EnrollmentRulesViolationException(String.format(msg, unitsRequested, gpa));
     }
 
     private int calcRequestedUnits(List<CSE> courses) {
@@ -39,34 +38,38 @@ public class EnrollCtrl {
     }
 
     private void checkDuplicateEnrollment(List<CSE> courses, CSE offering) throws EnrollmentRulesViolationException {
+        String msg = "%s is requested to be taken twice";
         for (CSE _offering : courses) {
             if (offering == _offering)
                 continue;
             if (offering.getCourse().equals(_offering.getCourse()))
-                throw new EnrollmentRulesViolationException(String.format("%s is requested to be taken twice", offering.getCourse().getName()));
+                throw new EnrollmentRulesViolationException(String.format(msg, offering.getCourse().getName()));
         }
     }
 
     private void checkExamTime(List<CSE> courses, CSE offering) throws EnrollmentRulesViolationException {
+        String msg = "Two offerings %s and %s have the same exam time";
         for (CSE _offering : courses) {
             if (offering == _offering)
                 continue;
             if (offering.getExamTime().equals(_offering.getExamTime()))
-                throw new EnrollmentRulesViolationException(String.format("Two offerings %s and %s have the same exam time", offering, _offering));
+                throw new EnrollmentRulesViolationException(String.format(msg, offering, _offering));
         }
     }
 
     private void checkPrerequisites(Student student, CSE offering) throws EnrollmentRulesViolationException {
+        String msg = "The student has not passed %s as a prerequisite of %s";
         List<Course> preReqs = offering.getCourse().getPrerequisites();
         for (Course preReq : preReqs) {
             if(!student.courseIsPassed(preReq))
-                throw new EnrollmentRulesViolationException(String.format("The student has not passed %s as a prerequisite of %s", preReq.getName(), offering.getCourse().getName()));
+                throw new EnrollmentRulesViolationException(String.format(msg, preReq.getName(), offering.getCourse().getName()));
         }
     }
 
     private void checkCourseIsPassed(Student student, CSE offering) throws EnrollmentRulesViolationException {
+        String msg = "The student has already passed %s";
         if(student.courseIsPassed(offering.getCourse()))
-            throw new EnrollmentRulesViolationException(String.format("The student has already passed %s", offering.getCourse().getName()));
+            throw new EnrollmentRulesViolationException(String.format(msg, offering.getCourse().getName()));
     }
 
 }
