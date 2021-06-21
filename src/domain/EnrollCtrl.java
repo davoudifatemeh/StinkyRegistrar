e4c748f70c8b9496b860d503563c5f12cb6cf962
Fatemeh Davoudi <70088942+davoudifatemeh@users.noreply.github.com>
2021-06-21 20:50:36 +0430

    Update EnrollCtrl.java
    Delete courseIsPassed, getGPA and submitEnrollment functions and move them to Student class

--- src/domain/EnrollCtrl.java
+++ src/domain/EnrollCtrl.java
@@ -8,45 +8,27 @@ import domain.exceptions.EnrollmentRulesViolationException;
 public class EnrollCtrl {
     public void enroll(Student s, List<CSE> courses) throws EnrollmentRulesViolationException {
         checkEnrollmentConditions(s, courses);
-        submitEnrollment(s, courses);
-    }
-
-    private void submitEnrollment(Student s, List<CSE> courses) {
-        for (CSE o : courses)
-            s.takeCourse(o.getCourse(), o.getSection());
+        s.submitEnrollment(courses);
     }
 
     private void checkEnrollmentConditions(Student s, List<CSE> courses) throws EnrollmentRulesViolationException {
         Map<Term, Map<Course, Double>> transcript = s.getTranscript();
         for (CSE o : courses) {
-            checkCourseIsPassed(transcript, o);
-            checkPrerequisites(transcript, o);
+            checkCourseIsPassed(s, o);
+            checkPrerequisites(s, o);
             checkExamTime(courses, o);
             checkDuplicateEnrollment(courses, o);
         }
-        checkUnitsLimit(courses, transcript);
+        checkUnitsLimit(courses, s);
     }
 
-    private void checkUnitsLimit(List<CSE> courses, Map<Term, Map<Course, Double>> transcript) throws EnrollmentRulesViolationException {
+    private void checkUnitsLimit(List<CSE> courses, Student s) throws EnrollmentRulesViolationException {
         int unitsRequested = calcRequestedUnits(courses);
-        double gpa = getGPA(transcript);
+        double gpa = s.getGPA();
         if ((gpa < 12 && unitsRequested > 14) ||
                 (gpa < 16 && unitsRequested > 16) ||
                 (unitsRequested > 20))
             throw new EnrollmentRulesViolationException(String.format("Number of units (%d) requested does not match GPA of %f", unitsRequested, gpa));
-    }
-
-    private double getGPA(Map<Term, Map<Course, Double>> transcript) {
-        double points = 0;
-        int totalUnits = 0;
-        for (Map.Entry<Term, Map<Course, Double>> tr : transcript.entrySet()) {
-            for (Map.Entry<Course, Double> r : tr.getValue().entrySet()) {
-                points += r.getValue() * r.getKey().getUnits();
-                totalUnits += r.getKey().getUnits();
-            }
-        }
-        double gpa = points / totalUnits;
-        return gpa;
     }
 
     private int calcRequestedUnits(List<CSE> courses) {
@@ -74,26 +56,17 @@ public class EnrollCtrl {
         }
     }
 
-    private void checkPrerequisites(Map<Term, Map<Course, Double>> transcript, CSE o) throws EnrollmentRulesViolationException {
+    private void checkPrerequisites(Student s, CSE o) throws EnrollmentRulesViolationException {
         List<Course> prereqs = o.getCourse().getPrerequisites();
-        nextPre:
         for (Course pre : prereqs) {
-            if(!courseIsPassed(transcript, pre))
+            if(!s.courseIsPassed(pre))
                 throw new EnrollmentRulesViolationException(String.format("The student has not passed %s as a prerequisite of %s", pre.getName(), o.getCourse().getName()));
         }
     }
 
-    private void checkCourseIsPassed(Map<Term, Map<Course, Double>> transcript, CSE o) throws EnrollmentRulesViolationException {
-        if(courseIsPassed(transcript, o.getCourse()))
+    private void checkCourseIsPassed(Student s, CSE o) throws EnrollmentRulesViolationException {
+        if(s.courseIsPassed(o.getCourse()))
             throw new EnrollmentRulesViolationException(String.format("The student has already passed %s", o.getCourse().getName()));
     }
-    public boolean courseIsPassed(Map<Term, Map<Course, Double>> transcript, Course o) {
-        for (Map.Entry<Term, Map<Course, Double>> tr : transcript.entrySet()) {
-            for (Map.Entry<Course, Double> r : tr.getValue().entrySet()) {
-                if (r.getKey().equals(o) && r.getValue() >= 10)
-                    return true;
-            }
-        }
-        return false;
-    }
+
 }
