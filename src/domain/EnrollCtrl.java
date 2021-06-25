34465e614a81446a541b15f1814149a4e097d163
Fatemeh Davoudi <70088942+davoudifatemeh@users.noreply.github.com>
2021-06-25 14:32:53 +0430

    Update EnrollCtrl.java
    Delete throw exceptions in checkCourseIsPassed، checkPrerequisites، checkExamTime، checkDuplicateEnrollment and checkUnitsLimit functions and replace them with printIn.
    Change the return value of this functions, enroll and checkEnrollmentConditions functions from void to boolean.

--- src/domain/EnrollCtrl.java
+++ src/domain/EnrollCtrl.java
@@ -2,32 +2,37 @@ package domain;
 
 import java.util.List;
 
-import domain.exceptions.EnrollmentRulesViolationException;
-
 public class EnrollCtrl {
-    public void enroll(Student student, List<CSE> courses) throws EnrollmentRulesViolationException {
-        checkEnrollmentConditions(student, courses);
-        student.submitEnrollment(courses);
+    public boolean enroll(Student student, List<CSE> courses) {
+        boolean result = checkEnrollmentConditions(student, courses);
+        if(result)
+            student.submitEnrollment(courses);
+        return result;
     }
 
-    private void checkEnrollmentConditions(Student student, List<CSE> courses) throws EnrollmentRulesViolationException {
+    private boolean checkEnrollmentConditions(Student student, List<CSE> courses) {
+        boolean result = true;
         for (CSE offering : courses) {
-            checkCourseIsPassed(student, offering);
-            checkPrerequisites(student, offering);
-            checkExamTime(courses, offering);
-            checkDuplicateEnrollment(courses, offering);
+            result = result && checkCourseIsPassed(student, offering);
+            result = result && checkPrerequisites(student, offering);
+            result = result && checkExamTime(courses, offering);
+            result = result && checkDuplicateEnrollment(courses, offering);
         }
-        checkUnitsLimit(courses, student);
+        result = result && checkUnitsLimit(courses, student);
+        return result;
     }
 
-    private void checkUnitsLimit(List<CSE> courses, Student student) throws EnrollmentRulesViolationException {
+    private boolean checkUnitsLimit(List<CSE> courses, Student student) {
         int unitsRequested = calcRequestedUnits(courses);
         String msg = "Number of units (%d) requested does not match GPA of %f";
         double gpa = student.getGPA();
         if ((gpa < 12 && unitsRequested > 14) ||
                 (gpa < 16 && unitsRequested > 16) ||
-                (unitsRequested > 20))
-            throw new EnrollmentRulesViolationException(String.format(msg, unitsRequested, gpa));
+                (unitsRequested > 20)) {
+            System.out.println(String.format(msg, unitsRequested, gpa));
+            return false;
+        }
+        return true;
     }
 
     private int calcRequestedUnits(List<CSE> courses) {
@@ -37,39 +42,51 @@ public class EnrollCtrl {
         return unitsRequested;
     }
 
-    private void checkDuplicateEnrollment(List<CSE> courses, CSE offering) throws EnrollmentRulesViolationException {
+    private boolean checkDuplicateEnrollment(List<CSE> courses, CSE offering) {
         String msg = "%s is requested to be taken twice";
         for (CSE _offering : courses) {
             if (offering == _offering)
                 continue;
-            if (offering.getCourse().equals(_offering.getCourse()))
-                throw new EnrollmentRulesViolationException(String.format(msg, offering.getCourse().getName()));
+            if (offering.getCourse().equals(_offering.getCourse())) {
+                System.out.println(String.format(msg, offering.getCourse().getName()));
+                return false;
+            }
         }
+        return true;
     }
 
-    private void checkExamTime(List<CSE> courses, CSE offering) throws EnrollmentRulesViolationException {
+    private boolean checkExamTime(List<CSE> courses, CSE offering) {
         String msg = "Two offerings %s and %s have the same exam time";
         for (CSE _offering : courses) {
             if (offering == _offering)
                 continue;
-            if (offering.getExamTime().equals(_offering.getExamTime()))
-                throw new EnrollmentRulesViolationException(String.format(msg, offering, _offering));
+            if (offering.getExamTime().equals(_offering.getExamTime())) {
+                System.out.println(String.format(msg, offering, _offering));
+                return false;
+            }
         }
+        return true;
     }
 
-    private void checkPrerequisites(Student student, CSE offering) throws EnrollmentRulesViolationException {
+    private boolean checkPrerequisites(Student student, CSE offering) {
         String msg = "The student has not passed %s as a prerequisite of %s";
         List<Course> preReqs = offering.getCourse().getPrerequisites();
         for (Course preReq : preReqs) {
-            if(!student.courseIsPassed(preReq))
-                throw new EnrollmentRulesViolationException(String.format(msg, preReq.getName(), offering.getCourse().getName()));
+            if(!student.courseIsPassed(preReq)) {
+                System.out.println(String.format(msg, preReq.getName(), offering.getCourse().getName()));
+                return false;
+            }
         }
+        return true;
     }
 
-    private void checkCourseIsPassed(Student student, CSE offering) throws EnrollmentRulesViolationException {
+    private boolean checkCourseIsPassed(Student student, CSE offering) {
         String msg = "The student has already passed %s";
-        if(student.courseIsPassed(offering.getCourse()))
-            throw new EnrollmentRulesViolationException(String.format(msg, offering.getCourse().getName()));
+        if(student.courseIsPassed(offering.getCourse())) {
+            System.out.println(String.format(msg, offering.getCourse().getName()));
+            return false;
+        }
+        return true;
     }
 
 }
